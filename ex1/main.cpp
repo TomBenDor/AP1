5a34b7af8dc1b237e0e6f574d6a7715bc92717b9
Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>
2021-07-31 22:33:18 +0300

    Data uploaded.

---
+++ ex1/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tom on 31/07/2021.
+//
+
+#include "main.h"
---
+++ ex1/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Tom on 31/07/2021.
+//
+
+#ifndef AP1_MAIN_H
+#define AP1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //AP1_MAIN_H
