eac0bd118589e2ba9625971c642be63004cd65b1
Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>
2021-09-10 17:51:38 +0300

    Now supporting multiple clients simultaneously.

--- ex3/server/main.cpp
+++ ex3/server/main.cpp
@@ -1,21 +1,51 @@
+#include <utility>
 #include <vector>
+#include <iostream>
 #include "Iris.h"
 #include "../utils.h"
 #include "KnnClassifier.h"
 #include "Classifier.h"
 #include "TCPServer.h"
+#include "pthread.h"
+
+void *handleClient(void *);
+
+class Config {
+public:
+    std::string path;
+    TCPServer *server;
+    int clientSock;
+
+    Config(std::string path, TCPServer *server, int clientSock) : path(std::move(path)), server(server),
+                                                                  clientSock(clientSock) {}
+};
 
 int main(int argc, char *argv[]) {
+    //Initialize the server according to the command line arguments
+    TCPServer server(INADDR_ANY, htons(55555));
+
+    std::vector<Config> configurations;
+
+    while (true) {
+        int clientSock = server.accept();
+
+        pthread_t tid;
+        std::string path = argv[1];
+        Config config(path, &server, clientSock);
+        configurations.push_back(config);
+        pthread_create(&tid, nullptr, handleClient, &configurations[configurations.size() - 1]);
+    }
+}
+
+void *handleClient(void *c) {
+    auto *config = (Config *) c;
     //Get the classified data
-    std::vector<Iris> classified = toIrisVector(readCSV(argv[1]));
+    std::vector<Iris> classified = toIrisVector(readCSV(config->path));
     //Initialize the classifier
     KnnClassifier<Iris> knnClassifier(classified, 5);
     Classifier<Iris> *classifier = &knnClassifier;
-    //Initialize the server according to the command line arguments
-    TCPServer server(INADDR_ANY, htons(55555));
-    int clientSock = server.accept();
-    //Receive the message
-    std::string msg = server.recv(clientSock);
+
+    std::string msg = config->server->recv(config->clientSock);
     //Get the indices of the irises
     std::vector<std::string> indices = split(msg, '\n');
     std::string types;
@@ -25,10 +55,6 @@ int main(int argc, char *argv[]) {
         types.append(classifier->classify(iris));
         types.append("\n");
     }
-    //Send the types back
-    server.send(clientSock, types);
-    //Close the server
-    server.close();
-    //Free memory
-    return 0;
+    config->server->send(config->clientSock, types);
+    return nullptr;
 }
