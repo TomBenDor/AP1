97eb132320a95e5fe9786e4f48b0d9b57dd5e82d
Omer Shoulstein <80311680+OmerShoulstein@users.noreply.github.com>
2021-09-14 20:49:07 +0300

    Update ex3/server/EuclideanDistance.h
    Co-authored-by: Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>

--- ex3/server/EuclideanDistance.h
+++ ex3/server/EuclideanDistance.h
@@ -11,7 +11,6 @@ template<class T>
 class EuclideanDistance : public Distance<T> {
 public:
     double distance(T t1, T t2) const override {
-        std::cout << "euc" << std::endl;
         std::vector<double> t1Coordinates = t1.getVector();
         std::vector<double> t2Coordinates = t2.getVector();
         if (t2Coordinates.size() != t1Coordinates.size()) {
