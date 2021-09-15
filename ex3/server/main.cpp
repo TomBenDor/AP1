2580e486bc26b1119deefe4d6cf40e8542bd6657
Tom Ben-Dor <36517134+TomBenDor@users.noreply.github.com>
2021-09-15 14:39:30 +0300

    Using std::thread instead of pthread.

--- ex3/server/main.cpp
+++ ex3/server/main.cpp
@@ -1,53 +1,38 @@
-#include <utility>
 #include <vector>
 #include "Iris.h"
 #include "../utils.h"
 #include "KnnClassifier.h"
 #include "Classifier.h"
 #include "TCPServer.h"
-#include "pthread.h"
+#include "thread"
 #include "EuclideanDistance.h"
 #include "ChebyshevDistance.h"
 #include "ManhattanDistance.h"
 
-void *handleClient(void *);
-
-class Config {
-public:
-    std::string path;
-    int clientSock;
-
-    Config(std::string path, int clientSock) : path(std::move(path)), clientSock(clientSock) {}
-};
+void handleClient(const std::string &path, int clientSock);
 
 int main(int argc, char *argv[]) {
     //Initialize the server according to the command line arguments
     TCPServer server(INADDR_ANY, htons(55555));
 
-    std::vector<Config> configurations;
-
     while (true) {
         int clientSock = server.accept();
 
-        pthread_t tid;
-        std::string path = argv[1];
-        Config config(path, clientSock);
-        configurations.push_back(config);
-        pthread_create(&tid, nullptr, handleClient, &configurations[configurations.size() - 1]);
+        std::thread handlingClient(handleClient, argv[1], clientSock);
+        handlingClient.detach();
     }
 }
 
-void *handleClient(void *c) {
-    auto *config = (Config *) c;
+void handleClient(const std::string &path, int clientSock) {
     //Get the classified data
-    std::vector<Iris> classified = toIrisVector(utils::readCSV(config->path), true);
+    std::vector<Iris> classified = toIrisVector(utils::readCSV(path), true);
     //Initialize the classifier
     ManhattanDistance<Iris> euclideanDistance;
     Distance<Iris> *distance = &euclideanDistance;
     KnnClassifier<Iris> knnClassifier(classified, 5, distance);
     Classifier<Iris> *classifier = &knnClassifier;
 
-    std::string msg = utils::recv(config->clientSock);
+    std::string msg = utils::recv(clientSock);
     //Get the indices of the irises
     std::vector<std::string> indices = utils::split(msg, '\n');
     std::string types;
@@ -57,6 +42,5 @@ void *handleClient(void *c) {
         types.append(classifier->classify(iris));
         types.append("\n");
     }
-    utils::send(config->clientSock, types);
-    return nullptr;
+    utils::send(clientSock, types);
 }
