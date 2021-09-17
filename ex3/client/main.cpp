8cc992f2c2dac87b50c47a3ba8a34a30c6a47fc2
Omer Shoulstein <omershoulstein@gmail.com>
2021-09-17 10:27:33 +0300

    Handling messages in the thread

--- ex3/client/main.cpp
+++ ex3/client/main.cpp
@@ -6,27 +6,16 @@
 #include "../utils.h"
 #include "TCPClient.h"
 #include "thread"
-#include "queue"
-#include "mutex"
-#include "condition_variable"
 
-std::condition_variable cv;
-std::mutex mutex;
-
-void receiving(std::queue<std::string> &receiving_queue, TCPClient client) {
-    while (true) {
-        std::string msg = client.recv();
-        receiving_queue.push(msg);
-        cv.notify_one();
-    }
+void handleMessage(const std::string &path, const std::string &msg) {
+    utils::writeCSV(path, utils::split(msg, '\n'));
 }
 
-std::string receiveFromServer(std::queue<std::string> &receiving_queue) {
-    std::unique_lock<std::mutex> uniqueLock(mutex);
-    cv.wait(uniqueLock, [&receiving_queue] { return !(receiving_queue.empty()); });
-    std::string res = receiving_queue.front();
-    receiving_queue.pop();
-    return res;
+void receiving(TCPClient client, std::string &path) {
+    while (true) {
+        std::string msg = client.recv();
+        handleMessage(path, msg);
+    }
 }
 
 
@@ -35,8 +24,8 @@ int main() {
     TCPClient client(inet_addr("127.0.0.1"), htons(55555));
     //Get parameters from user
     std::string parameters;
-    std::queue<std::string> msgQueue;
-    std::thread receivingThread(receiving, std::ref(msgQueue), client);
+    std::string path;
+    std::thread receivingThread(receiving, client, std::ref(path));
     receivingThread.detach();
     //Read the iris data from the csv file
     while (true) {
@@ -46,11 +35,10 @@ int main() {
             client.send("exit");
             break;
         }
+        path = params[1];
         std::vector<std::vector<std::string>> unclassified = utils::readCSV(params[0]);
         std::string msg = utils::joinVector(unclassified);
         client.send(msg);
-        std::string types = receiveFromServer(msgQueue);
-        utils::writeCSV(params[1], utils::split(types, '\n'));
     }
     client.close();
     return 0;
