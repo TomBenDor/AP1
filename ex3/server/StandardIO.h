dab6924a138923a3da76b1c950d80b0b77f6637a
Omer Shoulstein <omershoulstein@gmail.com>
2021-09-17 16:39:58 +0300

    Added StandardIO and fixed the encapsulation in DefaultIO

---
+++ ex3/server/StandardIO.h
@@ -0,0 +1,16 @@
+#include "DefaultIO.h"
+
+#ifndef CLIENT_STANDARDIO_H
+#define CLIENT_STANDARDIO_H
+
+
+class StandardIO : public DefaultIO {
+public:
+    void write(std::string string) override;
+
+    std::string read() override;
+
+};
+
+
+#endif //CLIENT_STANDARDIO_H
