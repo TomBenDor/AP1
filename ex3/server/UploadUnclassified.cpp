cc156516faaf2bea93af4cde23430764951f2225
Omer Shoulstein <omershoulstein@gmail.com>
2021-09-18 22:43:44 +0300

    UploadUnclassified now updates train and test vectors

--- ex3/server/UploadUnclassified.cpp
+++ ex3/server/UploadUnclassified.cpp
@@ -1,6 +1,5 @@
 #include "UploadUnclassified.h"
 #include "vector"
-#include "Iris.h"
 #include "../utils.h"
 
 
@@ -8,14 +7,15 @@ void UploadUnclassified::execute() {
     this->getDefaultIO()->write("Please upload your local train csv file");
     std::string path = this->getDefaultIO()->read();
     std::vector<std::vector<std::string>> file = utils::readCSV(path);
-    std::vector<Iris> trainIrises = toIrisVector(file, false);
+    *this->trainIrises = toIrisVector(file, false);
     this->getDefaultIO()->write("Upload Complete");
 
     this->getDefaultIO()->write("Please upload your local test csv file");
     path = this->getDefaultIO()->read();
     file = utils::readCSV(path);
-    std::vector<Iris> testIrises = toIrisVector(file, false);
+    *this->testIrises = toIrisVector(file, false);
     this->getDefaultIO()->write("Upload Complete");
 }
 
-UploadUnclassified::UploadUnclassified(DefaultIO *io) : Command("upload an unclassified csv data file", io) {}
+UploadUnclassified::UploadUnclassified(DefaultIO *io, std::vector<Iris> *testIrises, std::vector<Iris> *trainIrises) :
+        Command("upload an unclassified csv data file", io), testIrises(testIrises), trainIrises(trainIrises) {}
--- ex3/server/UploadUnclassified.h
+++ ex3/server/UploadUnclassified.h
@@ -1,14 +1,19 @@
 #include "Command.h"
+#include "vector"
+#include "Iris.h"
 
 #ifndef CLIENT_UPLOADUNCLASSIFIED_H
 #define CLIENT_UPLOADUNCLASSIFIED_H
 
 
 class UploadUnclassified : public Command {
+private:
+    std::vector<Iris> *trainIrises;
+    std::vector<Iris> *testIrises;
 public:
     void execute() override;
 
-    explicit UploadUnclassified(DefaultIO *io);
+    explicit UploadUnclassified(DefaultIO *io, std::vector<Iris> *testIrises, std::vector<Iris> *trainIrises);
 };
 
 
